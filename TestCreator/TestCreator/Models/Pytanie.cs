76e2feb04ca125874e18f3c23e6c8d5cdcbbed72
Stanislaw <221579@edu.p.lodz.pl>
2018-04-03 22:22:06 +0200

    dodanie pytania

---
+++ TestCreator/TestCreator/Models/Pytanie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestCreator.Models
+{
+    public class Pytanie
+    {
+        public long id_pytanie { get; set; }
+        public string pytanie { get; set; }
+        public string odpowiedz { get; set; }
+        public int typ { get; set; }
+    }
+}
