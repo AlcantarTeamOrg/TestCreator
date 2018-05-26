0e54a5d84c586d19326b373a45d0c2d3f1ea242a
mika-bro <37345020+mika-bro@users.noreply.github.com>
2018-05-26 18:51:50 +0200

    Poprawiony widok

---
+++ TestCreatorV2/TestCreatorWebSite/Controllers/ModeratorController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestCreatorWebSite.Controllers
+{
+    public class ModeratorController : Controller
+    {
+        // GET: Moderator
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: TestUsers/Details/5
+        public ActionResult Details(long? id)
+        {
+
+            return View();
+        }
+
+        public ActionResult Delete(long? id)
+        {
+
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
